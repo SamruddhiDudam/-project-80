45a5c8acb6739c4d89c73cdf51f99f6cd36b2d93
SamruddhiDudam <78253578+SamruddhiDudam@users.noreply.github.com>
2021-05-07 19:49:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function getParagraph1() {
     var inputs = [];
     for (var i = 1; i <= 6; i++ ){
-        inputs.push(document.getElementById("div1_input_box" + i).value);
+        inputs.push(document.getElementById("div1_input_box_" + i).value);
     }
     document.getElementById("showParagraph1").innerHTML = inputs.join(" .");
 }
@@ -12,7 +12,7 @@ function getParagraph1() {
 function getParagraph2() {
     var inputs = [];
     for (var i = 1; i <= 6; i++ ){
-        inputs.push(document.getElementById("div2_input_box" + i).value);
+        inputs.push(document.getElementById("div2_input_box_" + i).value);
     }
     document.getElementById("showParagraph2").innerHTML = inputs.join(" .");
 }
